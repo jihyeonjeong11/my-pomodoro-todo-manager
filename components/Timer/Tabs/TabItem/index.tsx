5a8a10b1e9dfacadd5133599c34d80e020b6c234
JIHYEON JEONG <jihyeonjeong@JIHYEONs-MacBook-Pro.local>
2024-06-12 15:34:20 +0900

    added framer

--- components/Timer/Tabs/TabItem/index.tsx
+++ components/Timer/Tabs/TabItem/index.tsx
@@ -3,20 +3,20 @@ import { SelectedTabType } from "@/types/Timer";
 import { StyledTimerButton } from "../../styled/StyledTimer";
 
 const TabItem: FC<{ selectedTitle: SelectedTabType }> = ({ selectedTitle }) => {
-  const { title } = usePomodoro(["title"]);
+	const { title } = usePomodoro(["title"]);
 
-  return (
-    <StyledTimerButton
-      role="button"
-      aria-pressed={title.get === selectedTitle}
-      checked={title.get === selectedTitle}
-      onClick={() => {
-        title.set(selectedTitle);
-      }}
-    >
-      <span>{selectedTitle}</span>
-    </StyledTimerButton>
-  );
+	return (
+		<StyledTimerButton
+			role="button"
+			aria-pressed={title.get === selectedTitle}
+			checked={title.get === selectedTitle}
+			onClick={() => {
+				title.set(selectedTitle);
+			}}
+		>
+			<span>{selectedTitle}</span>
+		</StyledTimerButton>
+	);
 };
 
 export default TabItem;
