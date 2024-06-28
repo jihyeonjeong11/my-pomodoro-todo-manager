09b6a84f7d1def8bafbc1afa168448f92169052f
JIHYEON JEONG <jihyeonjeong@JIHYEONs-MacBook-Pro.local>
2024-06-28 12:31:50 +0900

    figuring out how to handle new Set with refs

--- components/Timer/Tabs/TabItem/index.tsx
+++ components/Timer/Tabs/TabItem/index.tsx
@@ -2,43 +2,35 @@ import { MutableRefObject, useCallback } from "react";
 import { usePomodoro } from "@/components/contexts/PomodoroContext";
 import { SelectedTabType } from "@/types/Timer";
 import { StyledTimerButton } from "../../styled/StyledTimer";
-import { findTab } from "../../functions";
 
 const TabItem: FC<{
-  selectedTitle: SelectedTabType;
-  itemRefs: MutableRefObject<Set<HTMLButtonElement>>;
-}> = ({ selectedTitle, itemRefs }) => {
-  const { tab } = usePomodoro(["tab"]);
-  const isSelected = tab.get.title === selectedTitle;
+	selectedTitle: SelectedTabType;
+	itemRefs: MutableRefObject<Set<HTMLButtonElement>>;
+	onClick: any;
+}> = ({ selectedTitle, itemRefs, onClick }) => {
+	const { tab } = usePomodoro(["tab"]);
+	const isSelected = tab.get.title === selectedTitle;
 
-  const refCallback = useCallback(
-    (element: HTMLButtonElement | null) => {
-      if (element) {
-        itemRefs.current.add(element);
-      } else {
-        itemRefs.current.forEach((ref) => {
-          if (!ref.isConnected) {
-            itemRefs.current.delete(ref);
-          }
-        });
-      }
-    },
-    [itemRefs]
-  );
+	const refCallback = useCallback(
+		(element: HTMLButtonElement | null) => {
+			if (element) {
+				itemRefs.current.add(element);
+			} else {
+				itemRefs.current.forEach((ref) => {
+					if (!ref.isConnected) {
+						itemRefs.current.delete(ref);
+					}
+				});
+			}
+		},
+		[itemRefs]
+	);
 
-  return (
-    <StyledTimerButton
-      ref={refCallback}
-      role="button"
-      aria-pressed={isSelected}
-      checked={isSelected}
-      onClick={() => {
-        tab.set(findTab(selectedTitle));
-      }}
-    >
-      <span>{selectedTitle}</span>
-    </StyledTimerButton>
-  );
+	return (
+		<StyledTimerButton ref={refCallback} role="button" aria-pressed={isSelected} checked={isSelected} onClick={(e) => onClick(e, selectedTitle)}>
+			<span>{selectedTitle}</span>
+		</StyledTimerButton>
+	);
 };
 
 export default TabItem;
--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -1,27 +1,48 @@
-import { type MutableRefObject, useRef } from "react";
+import { type MutableRefObject, useRef, useCallback } from "react";
+import { type SelectedTabType } from "@/types/Timer";
 import Tabs from "./Tabs";
 import TabItem from "./Tabs/TabItem";
 import { TABS } from "./constants";
 import Clock from "./Clock";
+import { findTab, getFromSet } from "./functions";
+import { usePomodoro } from "../contexts/PomodoroContext";
 
 const Timer: FC = () => {
-  const timerRef = useRef<MutableRefObject<HTMLObjectElement>>(null);
-  const itemRefs: MutableRefObject<Set<HTMLButtonElement>> = useRef(new Set());
+	const timerRef = useRef<MutableRefObject<HTMLObjectElement>>(null);
+	const itemRefs: MutableRefObject<Set<HTMLButtonElement>> = useRef(new Set());
+	const {
+		tab: { set, get },
+	} = usePomodoro(["tab"]);
 
-  return (
-    <>
-      <Tabs ref={timerRef}>
-        {TABS.map((item, index) => (
-          <TabItem
-            itemRefs={itemRefs}
-            data-testid={`tab-item-${index}`}
-            key={item.title}
-            selectedTitle={item.title}
-          />
-        ))}
-      </Tabs>
-      <Clock />
-    </>
-  );
+	const onClick = useCallback(
+		(e: MouseEvent, selectedTitle: SelectedTabType) => {
+			const prev = get.title;
+			if (e?.currentTarget && itemRefs.current.has(e.currentTarget as HTMLButtonElement)) {
+				const found = getFromSet(itemRefs.current, e.currentTarget);
+				let prev = null;
+				for (const i of itemRefs.current) {
+					console.log(i.textContent, get.title);
+					if (i.textContext === get.title) {
+						console.log("hello");
+						prev = i;
+					}
+				}
+				console.log(prev);
+				set(findTab(selectedTitle));
+			}
+		},
+		[set]
+	);
+
+	return (
+		<>
+			<Tabs>
+				{TABS.map((item, index) => (
+					<TabItem onClick={onClick} itemRefs={itemRefs} data-testid={`tab-item-${index}`} key={item.title} selectedTitle={item.title} />
+				))}
+			</Tabs>
+			<Clock />
+		</>
+	);
 };
 export default Timer;
