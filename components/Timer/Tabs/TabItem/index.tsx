56c650020db734245f2835acdf76a5c7f49a435a
jihyeonjeong11 <wjdwlgus11@gmail.com>
2024-06-29 22:55:09 +0900

    nightly

--- components/Timer/Tabs/TabItem/index.tsx
+++ components/Timer/Tabs/TabItem/index.tsx
@@ -1,35 +1,40 @@
 import { MutableRefObject, useCallback } from "react";
 import { SelectedTabType } from "@/types/Timer";
-import { StyledTimerButton } from "../../styled/StyledTimer";
+import { StyledTimerHighlight } from "../../styled/StyledTimer";
 
 const TabItem: FC<{
-	selectedTitle: SelectedTabType;
-	itemRefs: MutableRefObject<Set<HTMLButtonElement>>;
-	onClick: any;
-	isSelected: boolean;
+  selectedTitle: SelectedTabType;
+  itemRefs: MutableRefObject<Set<HTMLButtonElement>>;
+  onClick: any;
+  isSelected: boolean;
 }> = ({ selectedTitle, itemRefs, onClick, isSelected }) => {
-	const refCallback = useCallback(
-		(element: HTMLButtonElement | null) => {
-			if (element) {
-				itemRefs.current.add(element);
-			} else {
-				itemRefs.current.forEach((ref) => {
-					if (!ref.isConnected) {
-						itemRefs.current.delete(ref);
-					}
-				});
-			}
-		},
-		[itemRefs]
-	);
+  const refCallback = useCallback(
+    (element: HTMLButtonElement | null) => {
+      if (element) {
+        itemRefs.current.add(element);
+      } else {
+        itemRefs.current.forEach((ref) => {
+          if (!ref.isConnected) {
+            itemRefs.current.delete(ref);
+          }
+        });
+      }
+    },
+    [itemRefs]
+  );
 
-	return (
-		<div className="tab-container">
-			<StyledTimerButton ref={refCallback} role="button" aria-pressed={isSelected} checked={isSelected} onClick={(e) => onClick(e, selectedTitle)}>
-				<span>{selectedTitle}</span>
-			</StyledTimerButton>
-		</div>
-	);
+  return (
+    <div className="tab-item">
+      <button
+        ref={refCallback}
+        role="button"
+        aria-pressed={isSelected}
+        onClick={(e) => onClick(e, selectedTitle)}
+      >
+        <span>{selectedTitle}</span>
+      </button>
+    </div>
+  );
 };
 
 export default TabItem;
--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -1,4 +1,10 @@
-import { type MutableRefObject, useRef, useCallback, useState, useEffect } from "react";
+import {
+  type MutableRefObject,
+  useRef,
+  useCallback,
+  useState,
+  useEffect,
+} from "react";
 import { type SelectedTabType } from "@/types/Timer";
 import Tabs from "./Tabs";
 import TabItem from "./Tabs/TabItem";
@@ -6,59 +12,100 @@ import { TABS } from "./constants";
 import Clock from "./Clock";
 import { findTab, getFromSet } from "./functions";
 import { usePomodoro } from "../contexts/PomodoroContext";
+import { StyledTimerHighlight } from "./styled/StyledTimer";
 
 const Timer: FC = () => {
-	const timerRef = useRef<MutableRefObject<HTMLObjectElement>>(null);
-	const itemRefs: MutableRefObject<Set<HTMLButtonElement>> = useRef(new Set());
-	const {
-		tab: { set, get },
-	} = usePomodoro(["tab"]);
+  const timerRef = useRef<MutableRefObject<HTMLObjectElement>>(null);
+  const itemRefs: MutableRefObject<Set<HTMLButtonElement>> = useRef(new Set());
+  const containerRef: MutableRefObject<HTMLDivElement> = useRef();
+  const {
+    tab: { set, get },
+  } = usePomodoro(["tab"]);
 
-	const [animationStyle, setAnimationStyle] = useState({});
+  const [animationStyle, setAnimationStyle] = useState({});
 
-	const onClick = useCallback(
-		(e: MouseEvent, selectedTitle: SelectedTabType) => {
-			if (e?.currentTarget && itemRefs.current.has(e.currentTarget as HTMLButtonElement)) {
-				const found = getFromSet(itemRefs.current, (ele) => ele === e.currentTarget);
-				const prev = getFromSet(itemRefs.current, (ele) => ele.textContent === get.title);
-				if (!found || !prev) {
-					throw new Error("must be element");
-				}
-				const transformRange = found.offsetLeft - prev.offsetLeft;
+  const onClick = useCallback(
+    (e: MouseEvent, selectedTitle: SelectedTabType) => {
+      if (
+        e?.currentTarget &&
+        itemRefs.current.has(e.currentTarget as HTMLButtonElement)
+      ) {
+        const found = getFromSet(
+          itemRefs.current,
+          (ele) => ele === e.currentTarget
+        );
+        const prev = getFromSet(
+          itemRefs.current,
+          (ele) => ele.textContent === get.title
+        );
+        if (!found || !prev) {
+          throw new Error("must be element");
+        }
+        // console.log(containerRef.current);
+        // // return;
+        // const pRect = prev.getBoundingClientRect();
+        // const cRect = containerRef.current.getBoundingClientRect();
+        // const tRect = found.getBoundingClientRect();
+        // console.log(tRect, pRect);
+        // console.log(tRect.left - pRect.left);
+        // const transformRange = tRect.left - cRect.left;
+        // console.log(transformRange);
 
-				setAnimationStyle({
-					transform: `translateX(${transformRange}px)`,
-					transition: "transform 0.3s ease-in-out",
-				});
+        // setAnimationStyle({
+        //   transform: `translateX(${tRect.left - pRect.left}px)`,
+        //   transition: "transform 0.3s ease-in-out",
+        // });
 
-				setTimeout(() => {
-					setAnimationStyle({});
-				}, 300);
+        const pRect = prev.getBoundingClientRect();
+        const tRect = found.getBoundingClientRect();
 
-				set(findTab(selectedTitle));
-			}
-		},
-		[set, get.title]
-	);
+        const translateX = tRect.x - pRect.x;
+        setAnimationStyle({
+          transform: `translateX(${tRect.left}px)`,
+          transition: "all 0.3s ease-in-out",
+        });
 
-	useEffect(() => {
-		const prevElement = getFromSet(itemRefs.current, (ele) => ele.textContent === get.title);
-		if (prevElement) {
-			Object.assign(prevElement.style, animationStyle);
-		}
-	}, [animationStyle, get.title]);
+        set(findTab(selectedTitle));
+      }
+    },
+    [set, get.title]
+  );
 
-	return (
-		<>
-			<Tabs>
-				{TABS.map((item, index) => {
-					const isSelected = item.title === get.title;
-					return <TabItem onClick={onClick} itemRefs={itemRefs} data-testid={`tab-item-${index}`} key={item.title} selectedTitle={item.title} isSelected={isSelected} />;
-				})}
-			</Tabs>
-			<Clock />
-		</>
-	);
+  //   useEffect(() => {
+  //     const prevElement = getFromSet(
+  //       itemRefs.current,
+  //       (ele) => ele.textContent === get.title
+  //     );
+  //     if (prevElement) {
+  //       Object.assign(prevElement.style, animationStyle);
+  //     }
+  //   }, [animationStyle, get.title]);
+
+  return (
+    <>
+      <nav ref={containerRef}>
+        {TABS.map((item, index) => {
+          const isSelected = item.title === get.title;
+          return (
+            <TabItem
+              onClick={onClick}
+              itemRefs={itemRefs}
+              data-testid={`tab-item-${index}`}
+              key={item.title}
+              selectedTitle={item.title}
+              isSelected={isSelected}
+            />
+          );
+        })}
+        <StyledTimerHighlight
+          className="tab-highlight"
+          style={animationStyle}
+        />
+      </nav>
+
+      <Clock />
+    </>
+  );
 };
 
 export default Timer;
