857c16f3fbdc889d39b1e762e809ff717775e704
jihyeonjeong11 <wjdwlgus11@gmail.com>
2024-07-21 13:42:38 +0900

    mobile padding adjustment

--- components/Timer/styled/StyledTimer.tsx
+++ components/Timer/styled/StyledTimer.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledTimer = styled.section`
-  padding: 5rem;
+  padding: 2rem;
   display: flex;
   flex-direction: column;
   align-items: center;
