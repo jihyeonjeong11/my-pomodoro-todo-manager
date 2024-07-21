282648eb8a272ff6538c037d060d4f7f4fea4ae8
jihyeonjeong11 <wjdwlgus11@gmail.com>
2024-07-21 12:13:01 +0900

    added basic from functionality

--- components/Timer/styled/StyledList.ts
+++ components/Timer/styled/StyledList.ts
@@ -1,35 +1,22 @@
 import { styled } from "styled-components";
 
 export const StyledList = styled.section`
-	color: white;
-	background-color: red;
-	/* position: absolute;
-	bottom: 6rem; */
-	height: 20vw;
-	color: white;
-	justify-content: center;
-	align-items: center;
+  margin-top: 2rem;
+  text-align: center;
+  color: white;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+
+  form {
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+
+    input {
+      text-align: center;
+    }
+  }
 `;
-
-// width: 80vw;
-// height: 80vw;
-
-// @media (min-width: 768px) {
-//   width: 53.38vw;
-//   height: 53.38vw;
-// }
-
-// @media (min-width: 992px) {
-//   width: 45vw;
-//   height: 45vw;
-// }
-
-// @media (min-width: 1200px) {
-//   width: 35vw;
-//   height: 35vw;
-// }
-
-// @media (min-width: 1400px) {
-//   width: 27vw;
-//   height: 27vw;
-// }
